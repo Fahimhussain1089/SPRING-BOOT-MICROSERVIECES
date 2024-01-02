fe3bfda2e714b3ca28510dcec6177111eb15c5b2
fahimhussain1089 <fahimhussain1089@gmail.com>
2024-01-02 19:41:29 +0530

    commit message

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(65,10001,'USD','INR','');
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(75,1002,'EUR','INR','');
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(25,1003,'AUD','INR','');
